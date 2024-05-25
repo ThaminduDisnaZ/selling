59a799c61eadece9043a360078aba3e7c44a825d
Thamindu Disna <156172744+ThaminduDisnaZ@users.noreply.github.com>
2024-05-26 04:54:08 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -18,21 +18,21 @@ function signup() {
       if (request.readyState == 4 & request.status == 200) {
          var response = request.responseText;
 
-if (response == "Success") {
-   document.getElementById("msg1").innerHTML = "Registration Successfully";
-   document.getElementById("msgdiv1").className = "alert alert-dismissible fade show py-2 bg-info d-block";
+         if (response == "Success") {
+            document.getElementById("msg1").innerHTML = "Registration Successfully";
+            document.getElementById("msgdiv1").className = "alert alert-dismissible fade show py-2 bg-info d-block";
 
 
-   fname.value = ".";
-   lname.value = "";
-   email.value = "";
-   mobile.value = "";
-   password.value = "";
+            fname.value = "";
+            lname.value = "";
+            email.value = "";
+            mobile.value = "";
+            password.value = "";
 
-} else {
-   document.getElementById("msg1").innerHTML = response;
-   document.getElementById("msgdiv1").className = "alert alert-dismissible fade show py-2 bg-warning d-block";
-}
+         } else {
+            document.getElementById("msg1").innerHTML = response;
+            document.getElementById("msgdiv1").className = "alert alert-dismissible fade show py-2 bg-warning d-block";
+         }
 
 
       }
@@ -42,33 +42,33 @@ if (response == "Success") {
 }
 
 
-function signin(){
+function signin() {
 
-var em = document.getElementById("em");
-var pw = document.getElementById("pw");
-var rm = document.getElementById("rm");
+   var em = document.getElementById("em");
+   var pw = document.getElementById("pw");
+   var rm = document.getElementById("rm");
 
-var f = new FormData();
-f.append("e",em.value);
-f.append("p",pw.value);
-f.append("r",rm.checked);
+   var f = new FormData();
+   f.append("e", em.value);
+   f.append("p", pw.value);
+   f.append("r", rm.checked);
 
-var request = new XMLHttpRequest();
+   var request = new XMLHttpRequest();
 
-request.onreadystatechange = function () {
+   request.onreadystatechange = function () {
 
-if (request.readyState == 4 & request.status == 200) {
-   var response = request.responseText;
+      if (request.readyState == 4 & request.status == 200) {
+         var response = request.responseText;
 
-   if (response == "Success") {
-      window.location = "index.php";
-   } else {
-      document.getElementById("msg2").innerHTML = response;
-      document.getElementById("msgdiv2").className = "alert alert-dismissible fade show py-2 bg-warning d-block";
-   }
-}
-};
-request.open("POST", "loginProcess.php", true);
-request.send(f);
+         if (response == "Success") {
+            window.location = "index.php";
+         } else {
+            document.getElementById("msg2").innerHTML = response;
+            document.getElementById("msgdiv2").className = "alert alert-dismissible fade show py-2 bg-warning d-block";
+         }
+      }
+   };
+   request.open("POST", "loginProcess.php", true);
+   request.send(f);
 
 }
